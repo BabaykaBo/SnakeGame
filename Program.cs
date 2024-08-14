2e188f6a2ef880ed9a1dcc814e1dfe7af37c5152
zaaqq135@gmail.com <Oleh>
2024-08-14 22:29:54 +0300

    added loop

--- Program.cs
+++ Program.cs
@@ -1,26 +1,46 @@
 ﻿using SnakeGame;
 
 Coord gridDimensions = new(50, 20);
-Coord snakePos = new(10,0);
+Coord snakePos = new(10, 2);
 
-for (int y = 0; y < gridDimensions.Y; y++)
+Random rand = new();
+Coord applePos = new(rand.Next(1, gridDimensions.X - 1), rand.Next(1, gridDimensions.Y - 1));
+
+int frameDelayMilSec = 100;
+
+Console.CursorVisible = false;
+
+while (true)
 {
-    for (int x = 0; x < gridDimensions.X; x++)
+    Console.Clear();
+
+    for (int y = 0; y < gridDimensions.Y; y++)
     {
-        Coord currentCoord = new(x, y);
-        
-        if (snakePos.Equals(currentCoord))
+        for (int x = 0; x < gridDimensions.X; x++)
         {
-            Console.Write("■");
+            Coord currentCoord = new(x, y);
+
+            if (snakePos.Equals(currentCoord))
+            {
+                Console.Write("■");
+            }
+            else if (applePos.Equals(currentCoord))
+            {
+                Console.Write("●");
+            }
+            else if (x == 0 || y == 0 || x == gridDimensions.X - 1 || y == gridDimensions.Y - 1)
+            {
+                Console.Write("#");
+            }
+            else
+            {
+                Console.Write(" ");
+            }
         }
-        else if (x == 0 || y == 0 || x == gridDimensions.X - 1 || y == gridDimensions.Y - 1)
-        {
-            Console.Write("#");
-        }
-        else
-        {
-            Console.Write(" ");
-        }
+        Console.WriteLine();
     }
-    Console.WriteLine();
+
+    Thread.Sleep(frameDelayMilSec);
 }
+
+Console.CursorVisible = true;
